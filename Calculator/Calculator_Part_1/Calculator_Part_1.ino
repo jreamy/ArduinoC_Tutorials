b7515caf24ed22a44ea59521f635437e42508ba7
jreamy <37675348+jreamy@users.noreply.github.com>
2018-04-28 13:06:47 -0700

    Add files via upload

---
+++ Calculator/Calculator_Part_1/Calculator_Part_1.ino
@@ -0,0 +1,68 @@
+/*
+ * In this file, write simple calculator functions:
+ *  - addition (example)
+ *  - subtraction
+ *  - multiplication
+ *  - division
+ *  - modulo
+ *  - exponent
+ *  - factorial
+ *  
+ *  Then in setup, print sample calculations to make sure your functions work
+ */
+
+
+void setup()
+{
+  // Begin Serial communication
+  Serial.begin(9600);
+  delay(100); // This is just generally good form for opening the Serial monitor
+  
+  // Test your functions here:
+  Serial.print("5 + 6 = ");
+  Serial.println(addition(5, 6));
+
+}
+
+
+// Don't do anything with the void loop for now
+void loop();
+
+// Addition
+int addition(int a, int b)
+{
+  return a + b;
+}
+
+// Subtraction
+int subtraction(int a, int b)
+{
+  
+}
+
+// Multiplication
+
+
+
+// Division
+
+
+
+// Modulo
+// Look up the modulo operator: "https://www.arduino.cc/reference/en/language/structure/arithmetic-operators/modulo/"
+// Use a while loop to achieve the same effect without using the "%" operator
+
+
+
+// Exponent
+// Use a for loop 
+
+
+
+// Factorial
+// Use a for loop
+
+
+
+
+
